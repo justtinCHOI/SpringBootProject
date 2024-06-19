e25117eb71559a884183cf5757dd96b277524b53
justtinCHOI <nicolao011113@gmail.com>
2024-06-19 17:13:01 +0900

    fix app.js 안녕하세요!!!

--- src/main/frontproject/src/App.js
+++ src/main/frontproject/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
     <div>
       <h3>hello react</h3>
       <p>안녕하세요!</p>
+      <h3>안녕하세요!! </h3>
     </div>
   );
 };
