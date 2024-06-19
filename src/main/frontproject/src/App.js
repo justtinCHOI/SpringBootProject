b48af734080d72a437a2d14d70d82d6d74701f48
justtinCHOI <nicolao011113@gmail.com>
2024-06-19 17:19:08 +0900

    hello react22

--- src/main/frontproject/src/App.js
+++ src/main/frontproject/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h3>hello react</h3>
+      <h3>hello react22</h3>
       <p>안녕하세요!</p>
       <h3>안녕하세요!!!</h3>
     </div>
